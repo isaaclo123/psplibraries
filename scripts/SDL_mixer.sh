283ee0db2cf48fa1aaafb69b503f222c733d001e
Isaac Lo <isaaclo123@gmail.com>
2021-02-15 12:23:15 -0800

    changes

--- scripts/SDL_mixer.sh
+++ scripts/SDL_mixer.sh
@@ -1,4 +1,7 @@
 test_deps_install SDL libmikmod
 get_pspport SDL_mixer SDL_mixer-psp
+# run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) \
+#  --disable-music-cmd --disable-music-mp3
+
 run_autogen_build --with-sdl-prefix=$(psp-config --psp-prefix) \
-  --disable-music-cmd --disable-music-mp3
+  --disable-music-cmd --disable-music-mp3 --disable-music-midi --disable-music-flac
